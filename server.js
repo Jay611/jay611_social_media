055fadf7f197016613cbe1ce0980fd58072342f4
Jaehan <jbae12@my.centennialcollege.ca>
2021-06-29 21:10:06 -0400

    auth controller

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Hello' });
-});
+app.use("/api", require("./routes/authRouter"));
 
 const URI = process.env.MONGODB_URL;
 mongoose.connect(
@@ -24,11 +22,11 @@ mongoose.connect(
   },
   (err) => {
     if (err) throw err;
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   }
 );
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log('Server is running on port', port);
+  console.log("Server is running on port", port);
 });
